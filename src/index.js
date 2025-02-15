596766ed01c4d838417c607852f98fdea7db8864
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-02-16 00:14:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const itineraryRouter = require("../routes/itinerary");
 const app = express();
 
 const corsOptions = {
-    origin: "gotravel-wine.vercel.app",
+    origin: ["gotravel-wine.vercel.app"],
     methods: "POST,GET,PUT,DELETE,PATCH,HEAD",
     credentials: true,
 };
